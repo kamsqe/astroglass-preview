5f42948ebc42f94a972fd52865e85ea8555623e6
kambar <mirmanoov@gmail.com>
2026-02-04 16:40:37 +0500

    refactor: switch to SSG for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
   vite: {
     plugins: [tailwindcss()]
   },
